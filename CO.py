7ade3ff2f9fbaec4f8d7b92ecbf28739ef3bb688
alone4 <nazhif14@gmail.com>
2023-07-06 04:35:46 +0700

    test 1

--- CO.py
+++ CO.py
@@ -1,13 +1,90 @@
 import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import streamlit as st
+from streamlit.web import bootstrap
+import datetime
+import streamlit as st
+import streamlit_modal as modal
+import streamlit.components.v1 as components
+from google.cloud import firestore
 
-def col3():
-    col1,col2,col3,col4 = st.columns(4)
-    with col1:
-                 st.write("")
-    with col2:
-                 st.write("")
-    with col3:
-                 st.write("")
-    with col4:
-                 st.write("")
-    return 
+st.title("Selamat datang apa yang anda perlukan hari ini?")
+st.write("click di bawah untuk memilih memasukkan orderan")
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestore-key.json")
+eks_choice = ["SAP","JNE"]
+metode_choice = ["COD","TRANSFER"]
+status_choice = ["Belum bayar (TF)", "Dikirim nanti", "Sudah bayar (tf)", "Pending", "Proses", "Cancel"]
+cs_by = ["salma","alya","salsa","intan"]
+
+
+def form_Wa():
+                with st.form("Masukkan orderan"):
+                    check = st.radio("apakah user sudah pernah membeli?", ("belum","sudah"))
+                    if check == "belum":
+                        col1,col2= st.columns(2)
+                        with col1:
+                            nama = st.text_input("nama customer")
+                            nama_wa = st.text_input("nama wa customer")
+                            no_hp = st.number_input("no hp")
+                        with col2:
+                            alamat = st.text_area("alamat")
+                            kota = st.text_input("kota")
+                            metode_pem = st.selectbox("pilih pembayaran", metode_choice)
+                        if st.form_submit_button("submit"):
+                                doc_input = db.collection("customer").document(nama_wa)
+                                doc_input.set({
+                                    "nama": nama,
+                                    "no_telp": no_hp,
+                                    "alamat": alamat,
+                                    "kota": kota
+                                })
+
+
+def modal_type_2(x):
+    return components.html(f"""<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Modal 1</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        {x}
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Modal 2</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Hide this modal and show the first with the button below.
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to first</button>
+      </div>
+    </div>
+  </div>
+</div>
+<a class="btn btn-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Open first modal</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>""", scrolling=True)
--- orderan.py
+++ orderan.py
@@ -5,55 +5,11 @@ import datetime
 import streamlit as st
 import streamlit_modal as modal
 import streamlit.components.v1 as components
+from CO import *
 from google.cloud import firestore
 
 st.title("Selamat datang apa yang anda perlukan hari ini?")
 st.write("click di bawah untuk memilih memasukkan orderan")
-components.html("""<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-  <body>
-  <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Modal 1</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Show a second modal and hide this one with the button below.
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Modal 2</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Hide this modal and show the first with the button below.
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to first</button>
-      </div>
-    </div>
-  </div>
-</div>
-<a class="btn btn-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Open first modal</a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
-</html>""", scrolling=True)
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("firestore-key.json")
 eks_choice = ["SAP","JNE"]
@@ -121,29 +77,9 @@ with cs:
                         min_date = datetime.datetime(2023,7,5)
                         max_date = datetime.date(2023,12,31)
             
-            with st.form("Masukkan orderan"):
-                    check = st.radio("apakah user sudah pernah membeli?", ("belum","sudah"))
-                    if check == "belum":
-                        col1,col2= st.columns(2)
-                        with col1:
-                            nama = st.text_input("nama customer")
-                            nama_wa = st.text_input("nama wa customer")
-                            no_hp = st.number_input("no hp")
-                        with col2:
-                            alamat = st.text_area("alamat")
-                            kota = st.text_input("kota")
-                            metode_pem = st.selectbox("pilih pembayaran", metode_choice)
-                        if st.form_submit_button("submit"):
-                                doc_input = db.collection("customer").document(nama_wa)
-                                doc_input.set({
-                                    "nama": nama,
-                                    "no_telp": no_hp,
-                                    "alamat": alamat,
-                                    "kota": kota
-                                })
-
-                                peng = st.selectbox("apakah dikirim nanti?", ["ya","tidak"])
-                                if metode_pem == "TRANSFER" and peng == "ya":
+                modal_type_2(form_Wa)
+                peng = st.selectbox("apakah dikirim nanti?", ["ya","tidak"])
+                if metode_pem == "TRANSFER" and peng == "ya":
                                     with modal.container():
                                         with st.form("Masukkan orderan"):
                                             status = st.selectbox("status orderan", status_choice[0,1,2])
