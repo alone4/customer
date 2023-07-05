4536b8c5f79b86ad562963f0e5caa6888eae5e45
alone4 <nazhif14@gmail.com>
2023-07-06 06:45:02 +0700

    Update orderan.py

--- orderan.py
+++ orderan.py
@@ -3,23 +3,8 @@ import streamlit as st
 from streamlit_modal import Modal
 import streamlit.components.v1 as components
 from google.cloud import firestore
-import datetime
 
-db = firestore.Client.from_service_account_json("firestore-key.json")
-eks_choice = ["SAP","JNE"]
-metode_choice = ["COD","TRANSFER"]
-status_choice = ["Belum bayar (TF)", "Dikirim nanti", "Sudah bayar (tf)", "Pending", "Proses", "Cancel"]
-cs_by = ["salma","alya","salsa","intan"]
-
-
-st.title("Selamat datang apa yang anda perlukan hari ini?")
-
-st.write("click di bawah untuk memilih memasukkan orderan")
-
-modal = Modal(key="Demo Modal", title="")
-
-
-
+modal = Modal("Demo Modal")
 open_modal = st.button("Open")
 if open_modal:
     modal.open()
@@ -40,5 +25,3 @@ if modal.is_open():
         st.write("Some fancy text")
         value = st.checkbox("Check me")
         st.write(f"Checkbox checked: {value}")
-
-                              
