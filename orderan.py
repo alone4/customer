18d833e0994f8c6c2167adfa3de51c2a7273db2e
alone4 <nazhif14@gmail.com>
2023-07-06 04:03:02 +0700

    Update orderan.py

--- orderan.py
+++ orderan.py
@@ -6,12 +6,31 @@ import streamlit as st
 import streamlit_modal as modal
 from google.cloud import firestore
 
-dial = st.dialog("Warning")
-dial.write("Here's a warning!")
+def dialog_close_button_clicked():
+    st.write("###  Dialog close callback results:")
+    st.write("#### No preferences saved!")
+    dialog.close()
 
-if st.button("Show warning"):
-    dial.open()
+dialog = st.dialog(
+    "first_dialog_with_help", title="Introduce yourself",
+    on_close_button_clicked=dialog_close_button_clicked, can_be_closed=True)
 
+def dialog_form_submit_button_clicked():
+    st.write("### Dialog form submit button clicked results:")
+    st.write(f"#### Hey {st.session_state.first_name} {st.session_state.last_name}!")
+    st.write(f"#### Your preferences are saved")
+    dialog.close()
+
+
+with dialog:
+    st.text_input("First name", key="first_name")
+    st.text_input("Last name", key="last_name")
+    st.checkbox("I accept cookies", key="cookies")
+    st.form_submit_button("OK", on_click=dialog_form_submit_button_clicked)
+
+
+if st.button("Open dialog", key="first_dialog_button"):
+    dialog.open()
 st.title("Selamat datang apa yang anda perlukan hari ini?")
 st.write("click di bawah untuk memilih memasukkan orderan")
 
