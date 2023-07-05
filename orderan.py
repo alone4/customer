2c4ccbf858846ddb419e5feee3d2b049597ca5f9
alone4 <nazhif14@gmail.com>
2023-07-06 01:21:17 +0700

    Update orderan.py

--- orderan.py
+++ orderan.py
@@ -15,7 +15,7 @@ db = firestore.Client.from_service_account_json("firestore-key.json")
 
 eks_choice = ["SAP","JNE"]
 metode_choice = ["COD","TRANSFER"]
-status_choice = ["belum bayar (TF)", "dikirim nanti", "sudah bayar (tf)", "pending"]
+status_choice = ["Belum bayar (TF)", "Dikirim nanti", "Sudah bayar (tf)", "Pending", "Proses", "Cancel"]
 cs_by = ["salma","alya","salsa","intan"]
 # Create a reference to the Google post.
 # Let's see what we got!
@@ -34,21 +34,35 @@ if modal.is_open():
                     nama = st.text_input("nama customer")
                     nama_wa = st.text_input("nama wa customer")
                     no_hp = st.number_input("no hp")
-                    barang = st.text_input("nama barang")
-                    jumlah_barang = st.number_input("jumlah barang")
                 with col2:
                     alamat = st.text_area("alamat")
                     kota = st.text_input("kota")
-                    ekspedisi = st.selectbox("pilih ekspedisi", eks_choice)
                     metode_pem = st.selectbox("pilih pembayaran", metode_choice)
-                status = st.selectbox("status orderan", status_choice)
-                harga_barang = st.number_input("harga barang awal")
-                diskon = st.number_input("jumlah diskon")
-                ongkir = st.number_input("biaya ongkir")
-                update_harga = harga_barang+ongkir-diskon
-                harga_akhir = st.number_input("harga akhir")
-                closing_by = st.selectbox("closing by", cs_by)
-                tanggal = datetime.datetime.now()
+                if st.form_submit_button("submit"):
+                        doc_input = db.collection("customer").document(nama_wa)
+                        doc_input.set({
+                            "nama": nama,
+                            "no_telp": no_hp,
+                            "alamat": alamat,
+                            "kota": kota
+                        })
+                        peng = st.selectbox("apakah dikirim nanti?", ["ya","tidak"])
+                        if metode_pem == "TRANSFER" and peng == "ya":
+                            with modal.container():
+                                with st.form("Masukkan orderan"):
+                                    status = st.selectbox("status orderan", status_choice[0,1,2])
+                                    ekspedisi = st.selectbox("pilih ekspedisi", eks_choice)
+                                    barang = st.text_input("nama barang")
+                                    jumlah_barang = st.number_input("jumlah barang")
+                                    status = st.selectbox("status orderan", status_choice)
+                                    harga_barang = st.number_input("harga barang awal")
+                                    diskon = st.number_input("jumlah diskon")
+                                    ongkir = st.number_input("biaya ongkir")
+                                    update_harga = harga_barang+ongkir-diskon
+                                    harga_akhir = st.number_input("harga akhir")
+                                    closing_by = st.selectbox("closing by", cs_by)
+                                    tanggal = datetime.datetime.now()
+
                 if st.form_submit_button("submit"):
                     doc_input = db.collection("customer").document(nama_wa).collection("orderan").document(f"{tanggal}")
                     doc_input.set({
