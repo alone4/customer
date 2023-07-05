682e6ae3a0e307e5da9c7475ea8d8f0195e68bb9
alone4 <32432759+alone4@users.noreply.github.com>
2023-07-05 01:06:53 -0400

    update

--- orderan.py
+++ orderan.py
@@ -26,7 +26,7 @@ if open_modal:
     modal.open()
 
 if modal.is_open():
-    with modal.container:
+    with modal.container():
         with st.form("Masukkan orderan"):
             check = st.radio("apakah user sudah pernah membeli?", ("belum","sudah"))
             if check == "belum":
