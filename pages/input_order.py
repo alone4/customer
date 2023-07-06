cfe93dce2cd9cbd119d86d9562e22a79d0b45b2c
alone4 <nazhif14@gmail.com>
2023-07-06 21:19:22 +0700

    update

--- pages/input_order.py
+++ pages/input_order.py
@@ -39,8 +39,8 @@ with st.form("Masukkan orderan"):
                                                 "alamat": alamat,
                                                 "kota": kota
                                             })
-                                peng= st.radio("apakah dikirim nanti?", ["ya","tidak"])
-                                while metode_pem == "TRANSFER" and peng == "tidak":
+                                    peng= st.radio("apakah dikirim nanti?", ["ya","tidak"])
+                                    while metode_pem == "TRANSFER" and peng == "tidak":
                                                     jenis_order = st.selectbox("Jenis orderan", jenis_order_choice)
                                                     barang = st.text_input("nama barang")
                                                     jumlah_barang = st.number_input("jumlah barang")
