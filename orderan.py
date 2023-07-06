65e578599e3cc703037e1d635e92827809af5c71
alone4 <nazhif14@gmail.com>
2023-07-06 20:14:08 +0700

    remove sidebar page

--- orderan.py
+++ orderan.py
@@ -8,7 +8,8 @@ from streamlit_extras.switch_page_button import switch_page
 from google.cloud import firestore
 st.title("Selamat datang apa yang anda perlukan hari ini?")
 st.write("click di bawah untuk memilih memasukkan orderan")
-
+st.set_page_config(initial_sidebar_state="collapsed") 
+st.markdown( """ <style> [data-testid="collapsedControl"] { display: none } </style> """, unsafe_allow_html=True, )
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("firestore-key.json")
 eks_choice = ["SAP","JNE"]
