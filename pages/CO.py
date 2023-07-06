8aa2ee82b2286cfbadd967234e299681a0bcfaab
alone4 <nazhif14@gmail.com>
2023-07-06 07:29:06 +0700

    Update CO.py

--- pages/CO.py
+++ pages/CO.py
@@ -34,39 +34,3 @@ with st.form("Masukkan orderan"):
                                                 "alamat": alamat,
                                                 "kota": kota
                                             })
-
-                                            peng = st.selectbox("apakah dikirim nanti?", ["ya","tidak"])
-                                            if metode_pem == "TRANSFER" and peng == "ya":
-                                                    with st.form("Masukkan orderan"):
-                                                        status = st.selectbox("status orderan", status_choice[0,1,2])
-                                                        ekspedisi = st.selectbox("pilih ekspedisi", eks_choice)
-                                                        barang = st.text_input("nama barang")
-                                                        jumlah_barang = st.number_input("jumlah barang")
-                                                        status = st.selectbox("status orderan", status_choice)
-                                                        harga_barang = st.number_input("harga barang awal")
-                                                        diskon = st.number_input("jumlah diskon")
-                                                        ongkir = st.number_input("biaya ongkir")
-                                                        update_harga = harga_barang+ongkir-diskon
-                                                        harga_akhir = st.number_input("harga akhir")
-                                                        closing_by = st.selectbox("closing by", cs_by)
-                                                        tanggal = datetime.datetime.now()
-
-                                                        if st.form_submit_button("submit"):
-                                                            doc_input = db.collection("customer").document(nama_wa).collection("orderan").document(f"{tanggal}")
-                                                            doc_input.set({
-                                                                    "nama": nama,
-                                                                    "no_telp": no_hp,
-                                                                    "alamat": alamat,
-                                                                    "kota": kota,
-                                                                    "barang": barang,
-                                                                    "jumlah_barang": jumlah_barang,
-                                                                    "ekspedisi": ekspedisi,
-                                                                    "metode pembayaran": metode_pem,
-                                                                    "status": status,
-                                                                    "harga_barang": harga_barang,
-                                                                    "diskon": diskon,
-                                                                    "ongkir": ongkir,
-                                                                    "harga_akhir": harga_akhir,
-                                                                    "closing_by": closing_by
-                                                        })
-
