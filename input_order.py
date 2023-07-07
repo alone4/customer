c323ce16960476bd47deaa8a567ba8d9e033c1a1
alone4 <nazhif14@gmail.com>
2023-07-07 09:00:57 +0700

    update input order to modal

--- input_order.py
+++ input_order.py
@@ -3,7 +3,6 @@ import streamlit as st
 from streamlit.web import bootstrap
 import datetime
 import streamlit.components.v1 as components
-from streamlit_extras.switch_page_button import switch_page
 from google.cloud import firestore
 def hides_side_page():
     st.set_page_config(initial_sidebar_state="collapsed") 
@@ -21,29 +20,32 @@ status_orderan_choice = [ "Pending", "Proses", "Cancel"]
 status_pembayaran_choice = ["Belum bayar (TF)", "Sudah bayar (tf)"]
 jenis_order_choice = ["fu tiktok","fu facebook","ro","fu cs"]
 cs_by = ["salma","alya","salsa","intan"]
-placeholder = st.empty()
-with placeholder.container():
-    with st.form("Masukkan orderan"):
-                                check = st.radio("apakah user sudah pernah membeli?", ("belum","sudah"))
-                                if check == "belum":
-                                    col1,col2= st.columns(2)
-                                    with col1:
-                                        nama = st.text_input("nama customer")
-                                        nama_wa = st.text_input("nama wa customer")
-                                        no_hp = st.number_input("no hp")
-                                    with col2:
-                                        alamat = st.text_area("alamat")
-                                        kota = st.text_input("kota")
-                                        metode_pem = st.selectbox("pilih pembayaran", metode_choice)
-                                    if st.form_submit_button("submit"):
-                                        doc_input = db.collection("customer").document(nama_wa)
-                                        doc_input.set({
-                                                "nama": nama,
-                                                "no_telp": no_hp,
-                                                "alamat": alamat,
-                                                "kota": kota
-                                            })
-                                        placeholder.empty()
+
+def input_pertama():
+    placeholder = st.empty()
+    with placeholder.container():
+        with st.form("Masukkan orderan"):
+                                    global nama,nama_wa,no_hp,alamat,kota,metode_pem
+                                    check = st.radio("apakah user sudah pernah membeli?", ("belum","sudah"))
+                                    if check == "belum":
+                                        col1,col2= st.columns(2)
+                                        with col1:
+                                            nama = st.text_input("nama customer")
+                                            nama_wa = st.text_input("nama wa customer")
+                                            no_hp = st.number_input("no hp")
+                                        with col2:
+                                            alamat = st.text_area("alamat")
+                                            kota = st.text_input("kota")
+                                            metode_pem = st.selectbox("pilih pembayaran", metode_choice)
+                                        if st.form_submit_button("submit"):
+                                            doc_input = db.collection("customer").document(nama_wa)
+                                            doc_input.set({
+                                                    "nama": nama,
+                                                    "no_telp": no_hp,
+                                                    "alamat": alamat,
+                                                    "kota": kota
+                                                })
+                                            placeholder.empty()
 
 peng = st.radio("apakah barang dikirim nanti?", ["ya","tidak"])
 if metode_pem == "TRANSFER" and peng == "tidak":
@@ -84,4 +86,3 @@ if metode_pem == "TRANSFER" and peng == "tidak":
                                                                     "closing_by": closing_by
                                                         })
                                                             st.success("data berhasil di masukkan")
-                                                            switch_page("orderan")
--- orderan.py
+++ orderan.py
@@ -6,6 +6,7 @@ import streamlit.components.v1 as components
 from streamlit_extras.switch_page_button import switch_page
 from streamlit_modal import Modal
 from google.cloud import firestore
+from input_order import *
 st.set_page_config(initial_sidebar_state="collapsed") 
 st.markdown( """ <style> [data-testid="collapsedControl"] { display: none } </style> """, unsafe_allow_html=True, )
 order = Modal(key="order modal", title="input order")
@@ -14,30 +15,7 @@ if open_modal:
     order.open()
 placeholder = st.empty()
 if order.is_open():
-    with order.container():
-      with placeholder.container():
-        with st.form("Masukkan orderan"):
-                                check = st.radio("apakah user sudah pernah membeli?", ("belum","sudah"))
-                                if check == "belum":
-                                    col1,col2= st.columns(2)
-                                    with col1:
-                                        nama = st.text_input("nama customer")
-                                        nama_wa = st.text_input("nama wa customer")
-                                        no_hp = st.number_input("no hp")
-                                    with col2:
-                                        alamat = st.text_area("alamat")
-                                        kota = st.text_input("kota")
-                                        metode_pem = st.selectbox("pilih pembayaran", metode_choice)
-                                    if st.form_submit_button("submit"):
-                                        doc_input = db.collection("customer").document(nama_wa)
-                                        doc_input.set({
-                                                "nama": nama,
-                                                "no_telp": no_hp,
-                                                "alamat": alamat,
-                                                "kota": kota
-                                            })
-                                        placeholder.empty()
-
+    input_pertama()
 st.title("Selamat datang apa yang anda perlukan hari ini?")
 st.write("click di bawah untuk memilih memasukkan orderan")
 
