bbbd6948e466600af4094d9793217760d72cf7eb
alone4 <nazhif14@gmail.com>
2023-07-07 14:06:36 +0700

    Update input_order.py

--- pages/input_order.py
+++ pages/input_order.py
@@ -11,6 +11,8 @@ st.set_page_config(initial_sidebar_state="collapsed")
 st.markdown( """ <style> [data-testid="collapsedControl"] { display: none } </style> """, unsafe_allow_html=True, )
 
 
+pass
+pass
 submit = st.form_submit_button("Submit")
 
 st.title("masukkan orderan")
