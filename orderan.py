d7222e2a537e93fa4e07202dbc77f98fbaaddd71
alone4 <nazhif14@gmail.com>
2023-07-07 09:46:36 +0700

    Update orderan.py

--- orderan.py
+++ orderan.py
@@ -17,7 +17,7 @@ if open_modal:
 placeholder = st.empty()
 if order.is_open():
     with order.container():
-        pass
+        input_kedua("COD","ya")
 
 st.title("Selamat datang apa yang anda perlukan hari ini?")
 st.write("click di bawah untuk memilih memasukkan orderan")
