dc7644c0a69f119e826b356315a0911fa9395bb3
alone4 <nazhif14@gmail.com>
2023-07-07 09:53:57 +0700

    Update input_order.py

--- pages/input_order.py
+++ pages/input_order.py
@@ -37,7 +37,7 @@ def input_pertama():
                                             kota = st.text_input("kota")
                                             metode_pem = st.selectbox("pilih pembayaran", metode_choice)
                                             st.radio("apakah barang dikirim nanti?", ["ya","tidak"])
-                                        if st.button("submit"):
+                                        if st.button("next"):
                                             doc_input = db.collection("customer").document(nama_wa)
                                             doc_input.set({
                                                     "nama": nama,
