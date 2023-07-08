daa40342a17960c385e915147a6069e64e38da66
alone4 <nazhif14@gmail.com>
2023-07-08 19:12:04 +0700

    Update orderan.py

--- orderan.py
+++ orderan.py
@@ -81,7 +81,7 @@ with cs:
                 for doc in docs:
                     st.write(f"{doc.id}")
 
-                users_ref1 = db.collection("customer").document("nazhif")
+                users_ref1 = db.collection("customer").document("nazhif").collection("orderan")
                 docs1 = users_ref1.stream()
                 for doc in docs1:
                     st.write(f"{doc.id}")
